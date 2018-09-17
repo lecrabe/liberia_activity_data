b71788b07a01f1e814302b76a61264ee4b63dbca
remi.dannunzio@gmail.com <remi.dannunzio@gmail.com>
2018-09-17 19:17:13 +0000

    fix projection issue

--- scripts/s2_combine_into_dd_map.R
+++ scripts/s2_combine_into_dd_map.R
@@ -23,15 +23,7 @@ time_start  <- Sys.time()
 # 
 # head(read.dbf(paste0(gadm_dir,"gadm_",countrycode,"_l1.dbf")))
 
-#################### Create a country boundary mask at the GFC resolution (TO BE REPLACED BY NATIONAL DATA IF AVAILABLE) 
-system(sprintf("python %s/oft-rasterize_attr.py -v %s -i %s -o %s -a %s",
-               scriptdir,
-               paste0(gadm_dir,"county_lisgis_0516.shp"),
-               paste0(gfc_dir,"gfc_treecover2000.tif"),
-               paste0(gadm_dir,"county_lisgis_0516.tif"),
-               "OBJECTID"
-))
-proj4string(shp)
+
 
 ####################################################################################
 ####### PREPARE COMMODITY MAP
@@ -89,14 +81,39 @@ system(sprintf("gdal_calc.py -A %s -B %s --co COMPRESS=LZW --outfile=%s --calc=\
 ))
 
 #################### COMBINATION INTO DD MAP (1==NF, 2==F, 3==Df, 4==Dg, 11==agriculture, 12==HCS_f, 13==HSC_df, 14==HSC_dg)
-system(sprintf("gdal_calc.py -A %s -B %s -C %s -D %s -E %s --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
+system(sprintf("gdal_calc.py -A %s -B %s -C %s -D %s --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
                paste0(dd_dir,"tmp_gfc_2004_gt",gfc_threshold,".tif"),
                paste0(dd_dir,"tmp_gfc_loss_0414_gt",gfc_threshold,"_sieve.tif"),
                paste0(dd_dir,"tmp_gfc_loss_0414_gt",gfc_threshold,"_inf.tif"),
-               paste0(gadm_dir,"gadm_",countrycode,"_l1.tif"),
                paste0(ag_dir,"commodities.tif"),
                paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,".tif"),
-               paste0("(D>0)*((A==0)*1+(A>0)*(E==0)*((B==0)*(C==0)*2+(B>0)*3+(C>0)*4)+(A>60)*(E>0)*((B==0)*(C==0)*12+(B>0)*13+(C>0)*14)+(E>0)*(A<60)*11)")
+               paste0("((A==0)*1+(A>0)*(D==0)*((B==0)*(C==0)*2+(B>0)*3+(C>0)*4)+(A>60)*(D>0)*((B==0)*(C==0)*12+(B>0)*13+(C>0)*14)+(D>0)*(A<60)*11)")
+))
+
+################################################################################
+#################### PROJECT IN UTM 29
+################################################################################
+system(sprintf("gdalwarp -t_srs \"%s\" -overwrite -ot Byte -co COMPRESS=LZW %s %s",
+               "EPSG:32629",
+               paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,".tif"),
+               paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,"_utm.tif")
+))
+
+#################### Create a country boundary mask at the GFC resolution (TO BE REPLACED BY NATIONAL DATA IF AVAILABLE) 
+system(sprintf("python %s/oft-rasterize_attr.py -v %s -i %s -o %s -a %s",
+               scriptdir,
+               paste0(gadm_dir,"county_lisgis_0516.shp"),
+               paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,"_utm.tif"),
+               paste0(gadm_dir,"county_lisgis_0516.tif"),
+               "OBJECTID"
+))
+
+#################### CLIP TO COUNTRY BOUNDARIES
+system(sprintf("gdal_calc.py -A %s -B %s  --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
+               paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,"_utm.tif"),
+               paste0(gadm_dir,"county_lisgis_0516.tif"),
+               paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,"_utm_country.tif"),
+               paste0("(B>0)*A")
 ))
 
 #################### CREATE A COLOR TABLE FOR THE OUTPUT MAP
@@ -118,7 +135,7 @@ write.table(pct,paste0(dd_dir,"color_table.txt"),row.names = F,col.names = F,quo
 ################################################################################
 system(sprintf("(echo %s) | oft-addpct.py %s %s",
                paste0(dd_dir,"color_table.txt"),
-               paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,".tif"),
+               paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,"_utm_country.tif"),
                paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,"pct.tif")
 ))
 
@@ -127,18 +144,8 @@ system(sprintf("(echo %s) | oft-addpct.py %s %s",
 ################################################################################
 system(sprintf("gdal_translate -ot Byte -co COMPRESS=LZW %s %s",
                paste0(dd_dir,"tmp_dd_map_0414_gt",gfc_threshold,"pct.tif"),
-               paste0(dd_dir,"dd_map_0414_gt",gfc_threshold,"_geo.tif")
-))
-
-################################################################################
-#################### PROJECT IN UTM 29
-################################################################################
-system(sprintf("gdalwarp -t_srs \"%s\" -ot Byte -co COMPRESS=LZW %s %s",
-               "EPSG:32629",
-               paste0(dd_dir,"dd_map_0414_gt",gfc_threshold,"_geo.tif"),
                paste0(dd_dir,"dd_map_0414_gt",gfc_threshold,"_utm.tif")
 ))
-
 
 
 
@@ -242,3 +249,4 @@ system(sprintf("rm %s",
 ))
 
 (time_decision_tree <- Sys.time() - time_start)
+
