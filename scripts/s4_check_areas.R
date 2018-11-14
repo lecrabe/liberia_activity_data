53c639e12956caa870928a76b210b135eecddcd6
lecrabe <remi.dannunzio@gmail.com>
2018-11-14 22:40:45 +0100

    distance to roads

--- scripts/s4_check_areas.R
+++ scripts/s4_check_areas.R
@@ -3,7 +3,8 @@ tif <- raster(paste0(pl_dir,"priority_areas_20181014.tif"))
 
 pls$areas <- gArea(pls,byid = T)
 head(pls)
-
+pls@data$areas/10000
+write.csv(pls@data,paste0(pl_dir,"areas_shp.csv"),row.names = F)
 system(sprintf("oft-stat %s %s %s",
                paste0(pl_dir,"priority_areas_20181014.tif"),
                paste0(pl_dir,"priority_areas_20181014.tif"),
@@ -19,3 +20,10 @@ tab$areas_shp <- tapply(pls$areas,pls$id,sum)
 tab$diff <- tab$areas_ras - tab$areas_shp
 tab
 write.csv(tab,paste0(pl_dir,"areas_comparison.csv"),row.names = F)
+
+
+test <- readOGR(paste0(pl_dir,"test_qgis_areas.shp"))
+
+test$area_r <- gArea(test,byid = T)/10000
+
+test@data
