4c4dcfe88ef529efa47dfb943304a97ba38f4aa5
remi.dannunzio@gmail.com <remi.dannunzio@gmail.com>
2019-09-25 08:52:39 +0000

    script for updated boundaries

--- scripts/ws_20180301_generate_map/s2_combine_into_dd_map_v4.R
+++ scripts/ws_20180301_generate_map/s2_combine_into_dd_map_v4.R
@@ -171,16 +171,16 @@ head(pls)
 #################### RASTERIZE THE PRIORITY LANDSCAPE
 system(sprintf("python %s/oft-rasterize_attr.py -v %s -i %s -o %s -a %s",
                scriptdir,
-               paste0(pl_dir,"priority_areas_20181014.shp"),
+               paste0(pl_dir,"priority_areas_20190925.shp"),
                paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_20181014.tif"),
-               paste0(pl_dir,"priority_areas_20181014.tif"),
+               paste0(pl_dir,"priority_areas_20190925.tif"),
                "id"
 ))
 
 #################### MASK MAP FOR PRIORITY LANDSCAPE 1
 system(sprintf("gdal_calc.py -A %s -B %s --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
                paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_20181014.tif"),
-               paste0(pl_dir,"priority_areas_20181014.tif"),
+               paste0(pl_dir,"priority_areas_20190925.tif"),
                paste0(dd_dir,"tmp_dd_map_0716_gt",gfc_threshold,"_utm_pl1.tif"),
                paste0("(B==1)*A")
 ))
@@ -188,7 +188,7 @@ system(sprintf("gdal_calc.py -A %s -B %s --co COMPRESS=LZW --outfile=%s --calc=\
 #################### MASK MAP FOR PRIORITY LANDSCAPE 2
 system(sprintf("gdal_calc.py -A %s -B %s --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
                paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_20181014.tif"),
-               paste0(pl_dir,"priority_areas_20181014.tif"),
+               paste0(pl_dir,"priority_areas_20190925.tif"),
                paste0(dd_dir,"tmp_dd_map_0716_gt",gfc_threshold,"_utm_pl2.tif"),
                paste0("(B==2)*A")
 ))
@@ -196,7 +196,7 @@ system(sprintf("gdal_calc.py -A %s -B %s --co COMPRESS=LZW --outfile=%s --calc=\
 #################### MASK MAP FOR NON PRIORITY LANDSCAPE
 system(sprintf("gdal_calc.py -A %s -B %s --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
                paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_20181014.tif"),
-               paste0(pl_dir,"priority_areas_20181014.tif"),
+               paste0(pl_dir,"priority_areas_20190925.tif"),
                paste0(dd_dir,"tmp_dd_map_0716_gt",gfc_threshold,"_utm_npl.tif"),
                paste0("(B==0)*A")
 ))
@@ -216,7 +216,7 @@ system(sprintf("(echo %s) | oft-addpct.py %s %s",
 ################################################################################
 system(sprintf("gdal_translate -ot Byte -co COMPRESS=LZW %s %s",
                paste0(dd_dir,"tmp_dd_map_0716_gt",gfc_threshold,"_utm_pl1_pct.tif"),
-               paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_pl1_20181014.tif")
+               paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_pl1_20190925.tif")
 ))
 
 ################################################################################
@@ -233,7 +233,7 @@ system(sprintf("(echo %s) | oft-addpct.py %s %s",
 ################################################################################
 system(sprintf("gdal_translate -ot Byte -co COMPRESS=LZW %s %s",
                paste0(dd_dir,"tmp_dd_map_0716_gt",gfc_threshold,"_utm_pl2_pct.tif"),
-               paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_pl2_20181014.tif")
+               paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_pl2_20190925.tif")
 ))
 
 ################################################################################
@@ -250,7 +250,7 @@ system(sprintf("(echo %s) | oft-addpct.py %s %s",
 ################################################################################
 system(sprintf("gdal_translate -ot Byte -co COMPRESS=LZW %s %s",
                paste0(dd_dir,"tmp_dd_map_0716_gt",gfc_threshold,"_utm_npl_pct.tif"),
-               paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_npl_20181014.tif")
+               paste0(dd_dir,"dd_map_0716_gt",gfc_threshold,"_utm_npl_20190925.tif")
 ))
 
 ################################################################################
