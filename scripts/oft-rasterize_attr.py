f64a2cdb856d81f6c7c6639037aad86671db01a6
lecrabe <remi.dannunzio@gmail.com>
2021-06-28 14:40:36 +0000

    oft-resterize update

--- scripts/oft-rasterize_attr.py
+++ scripts/oft-rasterize_attr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #This file is part of Open Foris Geospatial Toolkit which is free software.
 #You can redistribute it and/or modify it under the terms of the 
@@ -23,7 +23,7 @@
 #####################################################################################
 
 version="1.0"
-update ="16/10/2016" 
+update ="27/02/2020"
 
 import sys
 import subprocess
@@ -50,9 +50,9 @@ if argv is None:
 ###############################################################
 ##################### Test if all necessary arguments are in
 ###############################################################
-if len(argv) != 9:
-    print "\nVersion %s Last update on %s\nRasterize a shapefile on the grid of a given overlying raster\n" % (version,update)
-    print "Usage: oft-rasterize <-v vector_input> <-i mask_raster_input> <-o rasterized_output> [-a attribute]\n"
+if len(argv) > 9:
+    print("\nVersion %s Last update on %s\nRasterize a shapefile on the grid of a given overlying raster\n" % (version,update))
+    print("Usage: oft-rasterize <-v vector_input> <-i mask_raster_input> <-o rasterized_output> [-a attribute]\n")
     sys.exit( 0 )
 
 
@@ -105,9 +105,9 @@ outdir  = os.path.split(os.path.abspath(outfile))[0]
 ###############################################################
 ##################### Extract extent of image and pixel size
 ###############################################################
-ul = subprocess.check_output("gdalinfo %s | grep \"Upper Left\""  % infile, shell=True)
-lr = subprocess.check_output("gdalinfo %s | grep \"Lower Right\"" % infile, shell=True)
-px = subprocess.check_output("gdalinfo %s | grep \"Pixel Size\""  % infile, shell=True)
+ul = subprocess.check_output("gdalinfo %s | grep \"Upper Left\""  % infile, shell=True).decode("utf-8") 
+lr = subprocess.check_output("gdalinfo %s | grep \"Lower Right\"" % infile, shell=True).decode("utf-8") 
+px = subprocess.check_output("gdalinfo %s | grep \"Pixel Size\""  % infile, shell=True).decode("utf-8") 
 
 im_xmin = float(ul.split("(")[1].split(",")[0])
 im_ymax = float(re.sub(r'\)', '', ul.split("(")[1].split(",")[1]))
@@ -115,14 +115,14 @@ im_xmax = float(lr.split("(")[1].split(",")[0])
 im_ymin = float(re.sub(r'\)', '', lr.split("(")[1].split(",")[1]))
 pxsz    = float(px.split("(")[1].split(",")[0])
 
-print "-"*40
-print "Infile:  %s\nxmin : %r \nymax : %r \nxmax : %r \nymin : %r \nsize : %r\n" % (in_base,im_xmin,im_ymax,im_xmax,im_ymin,pxsz)
+print("-"*40)
+print("Infile:  %s\nxmin : %r \nymax : %r \nxmax : %r \nymin : %r \nsize : %r\n" % (in_base,im_xmin,im_ymax,im_xmax,im_ymin,pxsz))
 
 
 ###############################################################
 ##################### Extract extent of vector
 ###############################################################
-vc = subprocess.check_output("ogrinfo -al -so %s | grep \"Extent\"" % vector, shell=True)
+vc = subprocess.check_output("ogrinfo -al -so %s | grep \"Extent\"" % vector, shell=True).decode("utf-8") 
 vc=re.sub(r'Extent:\s','',vc)
 
 vc_xmin = float(re.sub(r'\(', '', vc.split(") - (")[0].split(",")[0]))
@@ -130,8 +130,8 @@ vc_ymin = float(re.sub(r'\)', '', vc.split(") - (")[0].split(",")[1]))
 vc_xmax = float(re.sub(r'\(', '', vc.split(") - (")[1].split(",")[0]))
 vc_ymax = float(re.sub(r'\)', '', vc.split(") - (")[1].split(",")[1]))
 
-print "-"*40
-print "Mask:    %s\nvc_xmin : %r \nvc_ymax : %r \nvc_xmax : %r \nvc_ymin : %r\n" % (vc_base,vc_xmin,vc_ymax,vc_xmax,vc_ymin)
+print("-"*40)
+print("Mask:    %s\nvc_xmin : %r \nvc_ymax : %r \nvc_xmax : %r \nvc_ymin : %r\n" % (vc_base,vc_xmin,vc_ymax,vc_xmax,vc_ymin))
 
 
 ################################################################
@@ -165,20 +165,20 @@ try:
     attr
     rasterize = "gdal_rasterize -a %s -l %s -ot Byte -te %r %r %r %r -tr %r %r -co \"COMPRESS=LZW\" %s %s/tmp_mask.tif\n" % (attr,layer,im_xmin,im_ymin,im_xmax,im_ymax,pxsz,pxsz,vector,tmpdir)
 except NameError:
-    print "-"*40 + "\nNo attribute specified, will clump all features of %s" % vector
-    rasterize = "gdal_rasterize -clump -l %s -ot Byte -te %r %r %r %r -tr %r %r -co \"COMPRESS=LZW\" %s %s/tmp_mask.tif\n" %   (layer,im_xmin,im_ymin,im_xmax,im_ymax,pxsz,pxsz,vector,tmpdir)
+    print("-"*40 + "\nNo attribute specified, will clump all features of %s" % vector)
+    rasterize = "gdal_rasterize -burn 1 -l %s -ot Byte -te %r %r %r %r -tr %r %r -co \"COMPRESS=LZW\" %s %s/tmp_mask.tif\n" %   (layer,im_xmin,im_ymin,im_xmax,im_ymax,pxsz,pxsz,vector,tmpdir)
 
 
 ############################################################### 
 ################ Rasterize the vector with at least one pixel encompassing the border
 ############################################################### 
-print "-"*40 + "\n" + rasterize
+print("-"*40 + "\n" + rasterize)
 os.system(rasterize)
 mask = tmpdir+"/tmp_mask.tif"
 
 ################# Compress and output
 compress = "gdal_translate -ot Byte -co \"COMPRESS=LZW\" %s/tmp_mask.tif %s" % (tmpdir,outfile) 
-print "-"*40 + "\n" + compress
+print("-"*40 + "\n" + compress)
 os.system(compress)
 
 
